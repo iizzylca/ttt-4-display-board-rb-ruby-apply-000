2ce2700e5000e81ce61243ae597ec8cb1a4a32ed
Israel Canessa <israellcanessa@gmail.com>
2020-05-06 05:27:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@ board[4]= "X"
 board[0]= "O"
 def display_board(board)
 
-puts "  #{board[0]}|   |   "
+puts " #{board[0]} |   |   "
 puts "-----------"
 puts "   | #{board[4]} |   "
 puts "-----------"
